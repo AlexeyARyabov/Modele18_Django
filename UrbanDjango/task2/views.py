9b225601c5c2993affbe86e4bf28cce6c8e2718d
AlexeyARyabov <98357572+AlexeyARyabov@users.noreply.github.com>
2024-12-19 23:32:33 +0300

    Add files via upload

--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -4,8 +4,8 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
-    return render(request, 'func_template.html')
+    return render(request, 'second_task/func_template.html')
 
 
 class Index2(TemplateView):
-    template_name = 'class_template.html'
+    template_name = 'second_task/class_template.html'
